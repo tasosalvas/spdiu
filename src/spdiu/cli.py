8e6f3471cb15d1997077084fdc5b0189d2951a5e
Tasos Alvas <tasos.alvas@qwertyuiopia.com>
2025-12-25 04:33:34 +0200

    siu accepts the install argument as a flag or dest string.

--- src/spdiu/cli.py
+++ src/spdiu/cli.py
@@ -7,6 +7,9 @@ from importlib import metadata
 from pathlib import Path
 
 from invoke import Program, Argument, Config
+from invoke.util import debug
+from invoke.exceptions import Exit
+
 from invoke.main import __version__ as invoke_version
 
 __version__ = metadata.version("spdiu")
@@ -30,30 +33,45 @@ class SpdIU(Program):
         extra_args = [
             Argument(
                 names=("install", "i"),
-                kind=bool,
+                kind=str,
+                default="",
+                optional=True,
                 help="Install SpdIU instance files in the current folder.",
             ),
         ]
         return core_args + extra_args
 
-    def print_version(self):
+    def parse_core(self, argv) -> None:
+        """Add SpdIU's base path installation to core args."""
+        super().parse_core(argv)
+
+        if self.args.install.value:
+            debug("Saw --install, generating SpdIU tasks.py")
+            i_value = self.args.install.value
+
+            if type(i_value) is str:
+                p = Path(i_value).resolve()
+            else:
+                p = Path.cwd()
+
+            p.mkdir(parents=True, exist_ok=True)
+            target = p / "tasks.py"
+            self.generate_spdiu_py(target)
+            raise Exit
+
+    def print_version(self) -> None:
         """Print the version of SpdIU and the underlying Invoke."""
         print(f"SpdIU {__version__} running on Invoke {invoke_version}")
 
-    def generate_spdiu_py(file_path):
+    def generate_spdiu_py(self, file_path) -> None:
         """Generate a tasks.py from spdiu/templates/tasks.py."""
         src = Path(__file__).parent / "templates" / "tasks.py"
+
         file_path.write_text(src.read_text())
+        print(f"Generated {file_path}")
 
 
 def main() -> None:
     """Install local configuration, otherwise invokes inv."""
     program = SpdIU(version=__version__, config_class=SpdIUConfig)
-
-    # cfg = Path.cwd() / 'invoke.yaml'
-
-    tasks = Path.cwd() / "tasks.py"
-    if not tasks.exists():
-        SpdIU.generate_spdiu_py(tasks)
-
     program.run()

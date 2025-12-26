c2f7a4efb6e0fc53ac04da09e144e1501e7ab8d3
Tasos Alvas <tasos.alvas@qwertyuiopia.com>
2025-12-26 04:37:30 +0200

    app version test, pypi keywords

---
+++ tests/10-app/test_app.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+# Copyright (C) 2025 Tasos Alvas <tasos.alvas@qwertyuiopia.com>
+# SPDX-License-Identifier: AGPL-3.0-or-later
+"""Tests for the s[pd]iu application."""
+
+from importlib import metadata
+from spdiu.cli import __version__
+
+
+def test_version() -> None:
+    """Test that the module's version is the one on pyproject."""
+    assert __version__ == metadata.version("spdiu")
